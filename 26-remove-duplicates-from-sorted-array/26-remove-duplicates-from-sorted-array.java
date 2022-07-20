e01e0d85b4bf0d0e5e9d821522b1faa90d18c6d7
Doha Ramadan <77820526+DohaRamadan@users.noreply.github.com>
2022-07-20 13:23:22 +0200

    Time: 1007 ms (5.00%), Space: 48.5 MB (6.96%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.java
@@ -1,14 +1,29 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        HashSet<Integer> set = new HashSet<Integer>();
+        Map<Integer, Integer> map = new HashMap<>();
+        HashSet<Integer> set = new HashSet<>();
         for(int i=0; i< nums.length; i++){
             set.add(nums[i]);
         }
         int k = set.size();
-        List<Integer> list = new ArrayList<Integer>(set);
-        Collections.sort(list);
-        for(int i=0; i< k ; i++){
-            nums[i] = list.get(i); 
+        int dup = nums.length - k, count=0;
+
+        int i =0;
+        while(i < nums.length){
+            int curr = nums[i];
+            int index = i;
+            if(count == dup)
+                break;
+            if(map.containsKey(curr)){
+                for(int j=index; j< nums.length - 1; j++){
+                    nums[j] = nums[j+1];
+                }
+                nums[nums.length - 1] = curr;
+                count++;
+            }else{
+                map.put(nums[i], 1);
+                i++;
+            }
         }
         return k;
     }
